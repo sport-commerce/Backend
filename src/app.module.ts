90282d731fa5330f0922287d9ebcbe4eef8f2c9c
YunYeoKyeong99 <ejmacdayori2e@gmail.com>
2023-11-08 12:20:25 +0900

    #1 feat(app): update environment variable configuration
    - Updated environment variable configuration in app.module.ts
    - Modified .gitignore, nest-cli.json, package.json, src/app.module.ts, src/main.ts
    - Refactored start:dev script

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [],
+  imports: 
+  [
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      isGlobal: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
